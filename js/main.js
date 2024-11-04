fdca561eaa6f3b9a081aa8119114c24b9982c7a1
NoorBaloch <126324818+apnadeveloper@users.noreply.github.com>
2024-11-04 09:55:54 +0500

    Upload the project

---
+++ js/main.js
@@ -0,0 +1,4 @@
+   // Toggle mobile menu visibility
+   document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.menu').classList.toggle('active');
+});
